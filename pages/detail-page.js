7540a9b2a7eb216637381bb10a205d4793860af8
Joey Grolleman <joeygrolleman@gmail.com>
2019-01-09 11:19:37 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,22 @@
 function submitComment() {
-    console.log('Hello!')
+    // Receivig the input
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+
+    // Creating the elements needed of the comment
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+   
+    // 
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+
+
+    console.log(comment);
   }
