70ff43f0da8ec54628cd87a81ef6f677764bf74a
Joey Grolleman <joeygrolleman@gmail.com>
2019-01-09 14:24:50 +0100

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -5,18 +5,24 @@ function submitComment() {
     const textArea = document.getElementById('msg');
     const msg = textArea.value;
 
-    // Creating the elements needed of the comment
+    // Creating the elements needed for the comment
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
    
-    // 
+    // Assigning input to the required elements
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
 
+    // Create new element.
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
 
-    console.log(comment);
+     // Reset the intput value of inputField and textArea.  
+    inputField.value = null
+    textArea.value = null
+
   }
